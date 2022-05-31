a1fff958151dc8d26a791bb4e9c010909d329e0b
Arzhh <romaric.herlin@arzhh.com>
2022-05-31 22:56:20 +0200

    init springboot base

---
+++ src/main/java/com/arzhh/menumaker/MenuMakerApplication.java
@@ -0,0 +1,13 @@
+package com.arzhh.menumaker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MenuMakerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MenuMakerApplication.class, args);
+	}
+
+}
